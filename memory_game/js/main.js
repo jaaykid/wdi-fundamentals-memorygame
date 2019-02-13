8679974a6fd6e99ab969f6ad60e8421516d53bfb
Justin Curry <justincurrydev@gmail.com>
2019-02-12 19:09:02 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,22 @@
-var cardOne = "king"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "queen"
+var cards = ["queen", "queen", "king", "king"] 
+var cardsInPlay = []
+var cardOne = [cards[3]]
+var cardTwo = [cards[0]]
+
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+
+console.log("User flipped " + cardOne)
+console.log("User flipped " + cardTwo )
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0][0] === cardsInPlay[1][0]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
+
+
+
+console.log(cardsInPlay)
